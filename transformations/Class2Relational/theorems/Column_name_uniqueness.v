b063e5ecc6bbca78283d48e69701cef1951dfcbe
Zheng Cheng <cz0590@gmail.com>
2022-12-15 21:07:29 +0800

    [refactor] simplify user theorem by removing unconstrained variables

--- transformations/Class2Relational/theorems/Column_name_uniqueness.v
+++ transformations/Class2Relational/theorems/Column_name_uniqueness.v
@@ -1,8 +1,7 @@
 (**
  CoqTL user theorem: Column_name_uniqueness
- Def: if all attributes of the same class have unique names,
-      then the generated columns of the same table
-      have unique names.
+ Def: if all attributes have unique names,
+      then the generated columns have unique names.
  **)
 
 Require Import Coq.Logic.Eqdep_dec.
@@ -29,27 +28,27 @@ forall (cm : ClassModel) (rm : RelationalModel),
     (* transformation *)
     rm = execute Class2Relational cm ->
     (* precondition *)
-    (forall (at1: Attribute_t) (at2: Attribute_t) (cl: Class_t) (*(ats: list Attribute_t)*),
+    (forall (at1: Attribute_t) (at2: Attribute_t)  (* (cl: Class_t) (ats: list Attribute_t)*),
         In (AttributeElement at1) cm.(modelElements) ->
         In (AttributeElement at2) cm.(modelElements) ->
-        In (ClassElement cl) cm.(modelElements) ->
-(*        getClassAttributes cl cm = Some ats ->
+(*         In (ClassElement cl) cm.(modelElements) ->
+       getClassAttributes cl cm = Some ats ->
         In at1 ats ->
         In at2 ats ->*)
         at1 <> at2 ->
         attr_name at1 <> attr_name at2) ->
     (* postcondition *)
-    (forall (co1: Column_t) (co2: Column_t) (ta: Table_t) (*(cos: list Column)*),
+    (forall (co1: Column_t) (co2: Column_t)  (*（ta: Table_t）(cos: list Column)*),
         In (ColumnElement co1) rm.(modelElements) ->
         In (ColumnElement co2) rm.(modelElements) ->
-        In (TableElement ta) rm.(modelElements) ->
-(*        getTableColumns ta rm = Some cos ->
+(*        In (TableElement ta) rm.(modelElements) ->
+        getTableColumns ta rm = Some cos ->
         In co1 cos ->
         In co2 cos ->*)
         co1 <> co2 ->
         column_name co1 <> column_name co2).
 Proof.
-    intros cm rm E PRE co1 co2 ta IN1 IN2 IN3 D.
+    intros cm rm E PRE co1 co2 IN1 IN2 D.
     subst rm.
 
     repeat Tactics.destruct_execute.
@@ -60,33 +59,26 @@ Proof.
     
     repeat Tactics.in_singleton_allTuples.
 
-    specialize (PRE a a0 c IN_E IN_E0 IN_E1). clear IN_E IN_E0  IN_E1.
+    specialize (PRE a a0 IN_E IN_E0). clear IN_E IN_E0.
 
-    (* IN3 *)
-    simpl in IN3.
-    remove_or_false IN3.
-    inversion_clear IN3. 
-    
-    
+    (* IN1 *)
+    destruct a.
+    replace derived with false in * ; 
+      [ | solve [destruct derived ; auto] ].
+    simpl in IN1.
+    remove_or_false IN1.
+    inversion IN1 ; clear IN1 ; subst. 
+
     (* IN2 *)
     destruct a0.
-    replace derived with false in *  ;
-      [ | solve [destruct derived ; auto] ].
+    replace derived0 with false in * ; 
+    [ | solve [destruct derived0 ; auto] ].
     simpl in IN2.
     remove_or_false IN2.
     inversion IN2 ; clear IN2 ; subst. 
 
-    
-    (* IN1 *)
-    destruct a.
-    replace derived0 with false in * ; 
-      [ | solve [destruct derived0 ; auto] ].            
-    simpl in IN1.
-    remove_or_false IN1.
-    inversion IN1 ; clear IN1 ; subst. 
-    
-    simpl. simpl in PRE.
-    
+    simpl.
+    simpl in PRE.
     apply PRE.
     contradict D.
     injection D ; clear D ; intros ; subst.
