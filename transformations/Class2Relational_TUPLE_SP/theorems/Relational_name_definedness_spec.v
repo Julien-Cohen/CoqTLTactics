ae074cae017bfa3a6bf16110ca94b3bc54653db8
Zheng Cheng <cz0590@gmail.com>
2023-01-20 13:55:00 +0800

    [refactor] add admitted theorems for class2relational variation

---
+++ transformations/Class2Relational_TUPLE_SP/theorems/Relational_name_definedness_spec.v
@@ -0,0 +1,147 @@
+(**
+ CoqTL user theorem: Relational_name_definedness
+ Def: if all objects in the source model have name defined,
+      then the target objects generated in the target model
+      have name defined. 
+ **)
+
+Require Import String.
+Require Import Coq.Logic.Eqdep_dec.
+Require Import Arith.
+Require Import Coq.Arith.Gt.
+Require Import Coq.Arith.EqNat.
+Require Import List.
+
+Require Import core.utils.Utils.
+Require Import core.SyntaxCertification.
+Require Import core.Metamodel.
+Require Import core.Model.
+Require Import core.Syntax.
+Require Import core.Engine.
+
+Require Import  core.Semantics.
+Require Import  core.Certification.
+
+
+Require Import transformations.Class2Relational_TUPLE_SP.Class2Relational_TUPLE_SP.
+Require Import transformations.Class2Relational_TUPLE_SP.ClassMetamodel.
+Require Import transformations.Class2Relational_TUPLE_SP.RelationalMetamodel.
+
+(* From transformations.Class2Relational Require Tactics. *)
+
+Theorem Relational_name_definedness (te: TransformationEngine CoqTLSyntax) (cm : ClassModel) (rm : RelationalModel) :
+  (* transformation *) 
+     rm = execute Class2Relational_TUPLE_SP cm ->
+  (* precondition *)   
+     (forall (c1 : ClassMetamodel.Element), 
+         In c1 cm.(modelElements) -> 
+            (ClassMetamodel.getName c1 <> ""%string)) ->
+  (* postcondition *) 
+     (forall (e : RelationalMetamodel.Element), 
+         In e rm.(modelElements) -> 
+            (RelationalMetamodel.getName e <> ""%string)). 
+Proof.
+  intros T P e IN; intros.
+  subst rm.
+
+  Tactics.destruct_execute.
+
+  (* Tactics.show_singleton. 
+
+  Tactics.in_singleton_allTuples.
+  specialize (P e0 IN_E). 
+  
+  repeat Tactics.destruct_any.
+
+  clear IN_I.
+
+  destruct e0. (* Case analysis on source element type *)
+  * (* [Class] *) 
+
+    Tactics.destruct_In_two ;
+      simpl in * ;
+      remove_or_false IN_OP ;
+      subst ope ; compute in IN ; 
+      [ Tactics.inj IN | discriminate IN ]. 
+      exact P.
+      
+  * (* [Attribute] *) 
+    destruct a.
+    destruct derived.
+    -- (* derived *)
+      exfalso. (* no rule can match *)
+      Tactics.destruct_In_two ; discriminate M.
+
+             
+    -- (* not derived *) 
+      Tactics.destruct_In_two;
+        simpl in * ;
+        remove_or_false IN_OP ;
+        subst ope ; compute in IN ;
+        [ discriminate IN | Tactics.inj IN ] .
+        exact P.
+Qed. *)
+
+Admitted.
+
+(* Alternative for (* [Attribute] *):
+      unfold instantiatePattern in H2. 
+      unfold matchPattern in H2.
+      unfold matchRuleOnPattern in H2. simpl in H2.
+      destruct (negb (getAttributeDerived c0)). 
+      -- simpl in H2. destruct H2. 
+        ++ rewrite <- H2. simpl. simpl in H0. assumption.
+        ++ contradiction H2. 
+      --  contradiction H2.*)
+
+
+
+(*Ltac destructPattern sp tr sm h := 
+  destruct sp;
+  [> contradiction | 
+     repeat
+      destruct sp;
+       [> 
+          | exfalso;
+            apply maxArity_length with (sp:=c::c0::x) (tr:=tr) (sm:=sm);
+            [> 
+              parseTransformationInGoal tr; unfold maxArity; simpl; omega |
+              assumption 
+            ]
+            +
+            destruct sp;
+       ]  
+  ].*)
+  
+(* 
+Ltac destruct_rule Hrule :=
+  repeat (destruct Hrule as [Hrule | Hrule]; try contradiction Hrule); destruct Hrule.
+
+Ltac destruct_pattern Hinst sp :=
+  repeat (let se := fresh "se" in
+          destruct sp as [ | se sp ];
+          [ | destruct se as [[] ?] eqn:?];
+          try contradiction Hinst);
+  destruct Hinst as [Hinst | []]; simpl in Hinst. *)
+
+(* 
+
+Theorem Relational_name_definedness':
+forall (cm : ClassModel) (rm : RelationalModel),
+  (* transformation *) rm = execute Class2Relational cm ->
+  (* precondition *)   (forall (c1 : ClassMetamodel_Element), In c1 (allModelElements cm) -> (ClassMetamodel_getName c1 <> ""%string)) ->
+  (* postcondition *)  (forall (t1 : RelationalMetamodel_Element), In t1 (allModelElements rm) -> (RelationalMetamodel_getName t1 <> ""%string)).
+Proof.
+  intros. subst rm.
+  destruct_execute H1 sp Hin Hinst. (* t1 comes from a pattern sp *)
+  apply allTuples_incl in Hin. (* sp is made of source model elements *)
+  destruct_instantiatePattern Hinst r Hr Hinst. (* sp matches a rule r *)
+  destruct_matchPattern Hr Hr Hmatch. (* r comes from the transformation *)
+  clear Hmatch. (* Hmatch is not used for this proof *)
+  destruct_rule Hr; (* case analysis on rules *)
+    destruct_pattern Hinst sp. (* retrieve the source pattern for each rule *)
+  - (* Class2Table *) specialize (H0 se). crush.
+  - (* Attribute2Column *) specialize (H0 se). crush.
+Qed.
+
+*)
---
+++ transformations/Class2Relational_TUPLE_SP/theorems/Table_name_uniqueness.v
@@ -0,0 +1,65 @@
+(**
+ CoqTL user theorem: Table_name_uniqueness
+ Def: if all classes in the source model have unique name,
+      then the target tables generated in the target model
+      have unique name.
+ **)
+
+Require Import Coq.Arith.Gt.
+Require Import Coq.Arith.EqNat.
+Require Import List.
+Require Import String.
+Require Import core.utils.Utils.
+
+Require Import core.Semantics.
+Require Import core.Certification.
+Require Import core.modeling.ModelingMetamodel.
+Require Import core.Model.
+
+Require Import transformations.Class2Relational_TUPLE_SP.Class2Relational_TUPLE_SP.
+Require Import transformations.Class2Relational_TUPLE_SP.ClassMetamodel.
+Require Import transformations.Class2Relational_TUPLE_SP.RelationalMetamodel.
+
+(* From transformations.Class2Relational Require Tactics. *)
+
+Theorem Table_name_uniqueness:
+forall (cm : ClassModel) (rm : RelationalModel), 
+(* transformation *) 
+    rm = execute Class2Relational_TUPLE_SP cm ->
+(* precondition *)   
+(forall (c1: Class_t) (c2: Class_t), 
+    In (ClassElement c1) cm.(modelElements) -> 
+    In (ClassElement c2) cm.(modelElements) -> 
+    c1 <> c2 -> 
+    class_name c1 <> class_name c2) ->
+(* postcondition *)  
+(forall (t1: Table_t) (t2: Table_t), 
+    In (TableElement t1) rm.(modelElements) -> 
+    In (TableElement t2) rm.(modelElements) -> 
+    t1 <> t2 -> 
+    table_name t1 <> table_name t2).
+Proof.
+    intros cm rm E PRE t1 t2 IN1 IN2 D.
+    subst rm.
+
+    repeat Tactics.destruct_execute.
+
+
+(*
+    repeat Tactics.show_singleton.
+
+    repeat Tactics.show_origin.
+
+    repeat Tactics.unify_all.
+    simpl.
+
+    repeat Tactics.in_singleton_allTuples.
+
+    specialize (PRE c c0 IN_E IN_E0) ; clear IN_E IN_E0.
+    apply PRE.
+    contradict D.
+    subst ; reflexivity.
+Qed.
+*)
+Admitted.
+
