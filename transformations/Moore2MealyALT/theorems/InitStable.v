4df6195aa79e8c35da03bc70485953167ad3de40
Julien Cohen <julien>
2023-06-23 16:31:52 +0200

    Progress.

---
+++ transformations/Moore2MealyALT/theorems/InitStable.v
@@ -0,0 +1,62 @@
+Require Moore2MealyALT.Moore2Mealy.
+Require Moore2MealyALT.theorems.Elements.
+Require Moore2MealyALT.theorems.WFStable.
+
+(*Import String OptionUtils.*)
+
+Lemma initial_state_preserved_fw2 : 
+      forall m s,
+        MooreWF.unique_names m ->
+        MooreSemantics.initialState m = Some s ->
+        MealySemantics.initialState (Semantics.execute Moore2Mealy.Moore2Mealy m) = Some (Elements.convert s).
+Proof.
+  intros m s WF INIT.
+  unfold MooreSemantics.initialState in INIT.
+  unfold MealySemantics.initialState.
+  apply OptionListUtils.find_lift_some in INIT. destruct INIT as (v & G & IN & E).
+  destruct v ; [ PropUtils.inj G | discriminate G].  (* monadInv *)
+  apply Id.internal_NodeId_dec_bl in E.
+  destruct s.
+  unfold Moore.State_name in E.
+  subst.
+  apply MealyWF.in_find ; [ apply MealyWF.always_unique_names | | reflexivity ].
+  apply Elements.state_element_fw.  exact IN.
+Qed.
+
+Lemma initial_state_preserved_fw3 : 
+      forall m,
+        MooreSemantics.initialState m = None ->
+        MealySemantics.initialState (Semantics.execute Moore2Mealy.Moore2Mealy m) = None.
+Proof.
+  unfold MooreSemantics.initialState, MealySemantics.initialState.
+  intros m INIT.
+  apply OptionListUtils.find_none.
+  intros x H.
+
+  destruct x ; [ right | left ; reflexivity].
+  unfold Mealy.get_E_data.
+  exists s ; split ; [ reflexivity | ].
+  eapply Elements.state_element_bw in H.
+  destruct H as (? & ? & ?). 
+  subst.
+  eapply OptionListUtils.find_none in INIT ; [ | exact H].
+
+  unfold Moore.get_E_data in INIT.
+  destruct INIT ; [ discriminate | ].
+  destruct H0 as (? & ? & ?). PropUtils.inj H0.
+  destruct x0 ; unfold Elements.convert, Mealy.State_name.
+  unfold Moore.State_name in *.
+  exact H1.
+Qed.
+
+Lemma initial_state_preserved : 
+  forall m,
+    MooreWF.unique_names m ->
+    MealySemantics.initialState (Semantics.execute Moore2Mealy.Moore2Mealy m) = option_map Elements.convert (MooreSemantics.initialState m).
+Proof.
+  intros m WD.
+  destruct (MooreSemantics.initialState m) eqn:I ; unfold option_map.
+  apply initial_state_preserved_fw2 ; assumption.
+  apply initial_state_preserved_fw3 ; assumption.
+  (* fixme : remove the two intermediate lemmas *)
+Qed.
