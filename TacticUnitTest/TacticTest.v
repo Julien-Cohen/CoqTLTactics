564289018a4c9e7d0e72207c88c6815e33b524ea
Julien Cohen <julien>
2024-01-30 20:06:25 +0100

    Résultats des tests sont non bloquants + log des tests.

--- TacticUnitTest/TacticTest.v
+++ TacticUnitTest/TacticTest.v
@@ -6,36 +6,73 @@ Require TacticsFW.
 Import 
   List Model String NotationUtils Semantics PoorTraceLink  NotationUtils Semantics.
 
+Import BasicMetamodel.
 
+Ltac test_success := idtac "Test succeeded.".
+Ltac test_failure := 
+  idtac " ";
+  idtac "========================" ;
+  idtac "===== Test Failed. =====" ;
+  idtac "========================" ;
+  idtac " ".
+
+
+(** Tactic under test : [TacticsFW.in_compute_trace_inv_singleton_fw] *)
+
+(* Test 1. *)
 Goal 
   forall (m : BasicMetamodel.M)
          (s : BasicMetamodel.Node_t)
-         (IN1 : In (BasicMetamodel.Node s) (modelElements m)),
+         (IN1 : In (Node s) (modelElements m)),
   exists p, 
      @In (@TraceLink.TraceLink IdTransformationConfiguration)
       {|
-        TraceLink.source := ([BasicMetamodel.Node s], 0, "s"%string);
-        TraceLink.produced := BasicMetamodel.Node s;
+        TraceLink.source := ([Node s], 0, "s"%string);
+        TraceLink.produced := Node s;
         TraceLink.linkPattern := p
       |} (compute_trace T m)
 .
 Proof.
-
+  idtac "Testing TacticsFW.in_compute_trace_inv_singleton_fw".
+  idtac "Test case : the convenient assumption is in the context.".
 
   intros. eexists.
 
-  Succeed solve [TacticsFW.in_compute_trace_inv_singleton_fw 1 1].
+(* Success of the tactid expected *)
+  Succeed (first [
+      solve [TacticsFW.in_compute_trace_inv_singleton_fw 1 1] ; 
+      test_success
+    | test_failure]).
 
-  Fail TacticsFW.in_compute_trace_inv_singleton_fw 2 1.
-  Fail TacticsFW.in_compute_trace_inv_singleton_fw 1 2.
-  Fail TacticsFW.in_compute_trace_inv_singleton_fw 0 0.
-  Fail TacticsFW.in_compute_trace_inv_singleton_fw 0 1.
-  Fail TacticsFW.in_compute_trace_inv_singleton_fw 1 0.
+(* Failure of the tactic expected *)
+ Succeed first [
+      TacticsFW.in_compute_trace_inv_singleton_fw 2 1 ; 
+      test_failure 
+    | test_success].
 
+ Succeed first [
+      TacticsFW.in_compute_trace_inv_singleton_fw 1 2 ; 
+      test_failure 
+    | test_success].
+
+ Succeed first [
+      TacticsFW.in_compute_trace_inv_singleton_fw 0 0 ; 
+      test_failure 
+    | test_success].
+ 
+ Succeed first [
+      TacticsFW.in_compute_trace_inv_singleton_fw 0 1 ; 
+      test_failure 
+    | test_success].
+
+ Succeed first [
+      TacticsFW.in_compute_trace_inv_singleton_fw 1 0 ; 
+      test_failure 
+    | test_success].
 
 Abort.
 
-Import BasicMetamodel.
+
 
 Goal
 forall 
@@ -50,8 +87,43 @@ forall
   In (BasicMetamodel.Node {| Node_id := 2 |})
     (modelElements (execute T cm)).
 Proof.
+  idtac "Testing TacticsFW.in_modelElements_singleton_fw_tac".
+  idtac "Test case : choice between two assumptions (1/2).".
+
   intros.
-  Succeed solve [TacticsFW.in_modelElements_singleton_fw_tac 1 1 0].
+
+  (* Success of the tactid expected *)
+  Succeed (first [
+      solve [TacticsFW.in_modelElements_singleton_fw_tac 1 1 0] ; 
+      test_success
+    | test_failure]).
+
+Abort.
+
+Goal
+forall 
+  (cm : BasicMetamodel.M)
+  
+  (H1 : In
+          (BasicMetamodel.Node {| Node_id := 2 |})
+          (modelElements cm))
+  (H2 : In (BasicMetamodel.Node {| Node_id := 1 |})
+         (modelElements cm)),
+
+  In (BasicMetamodel.Node {| Node_id := 2 |})
+    (modelElements (execute T cm)).
+Proof.
+  idtac "Testing TacticsFW.in_modelElements_singleton_fw_tac".
+  idtac "Test case : choice between two assumptions (2/2).".
+
+  intros.
+
+  (* Success of the tactid expected *)
+  Succeed (first [
+      solve [TacticsFW.in_modelElements_singleton_fw_tac 1 1 0] ; 
+      test_success
+    | test_failure]).
+
 Abort.
 
 
@@ -75,11 +147,20 @@ Goal
         TraceLink.linkPattern := lp
       |} (compute_trace T cm)
 .
+Proof.
+  idtac "Testing TacticsFW.in_compute_trace_inv_singleton_fw".
+  idtac "Test case : choice between two assumptions. (1/2)".
+  
+  eexists ; eexists ; eexists. 
 
-      eexists ; eexists ; eexists. 
+  (* Success of the tactid expected *)
+  Succeed (first [
+      solve [TacticsFW.in_compute_trace_inv_singleton_fw 1 1] ; 
+      test_success
+    | test_failure]).
 
-      Succeed solve [TacticsFW.in_compute_trace_inv_singleton_fw 1 1]. 
 Abort.
+
 
 Goal forall (cm : M)
   (H1 : In (Node {| Node_id := 2 |}) (modelElements cm))
@@ -96,15 +177,24 @@ Goal forall (cm : M)
         TraceLink.linkPattern := lp
       |} (compute_trace T cm)
 .
+Proof.
+  idtac "Testing TacticsFW.in_compute_trace_inv_singleton_fw".
+  idtac "Test case : choice between two assumptions. (2/2)".
 
       eexists ; eexists ; eexists. 
 
-   (* FIXME *)   progress TacticsFW.in_compute_trace_inv_singleton_fw 1 1. 
+  (* Success of the tactid expected *)
+  
+      TacticsFW.in_compute_trace_inv_singleton_fw 1 1 .
+
       compute.
       f_equal.
       f_equal.
       f_equal. (* The wrong assumption has been selected, and the goal cannot be solved anymore. *)
-      
+
+      first [reflexivity ; test_success | test_failure].
+
+      (* this failure is solved with the adapted tactic [TacticsFW.in_compute_trace_inv_singleton_fw_solve], see below. *)
 Abort.
 
 
@@ -125,16 +215,36 @@ Goal forall (cm : M)
         TraceLink.linkPattern := lp
       |} (compute_trace T cm)
 .
+Proof.
+  idtac "Testing TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered".
+  idtac "Test case : choice between two assumptions. (1/2)".
+
 
       eexists ; eexists ; eexists. 
 
-   Succeed  solve [TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered 1 1]. 
-   Fail  solve [TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered 2 1]. 
-Fail  solve [TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered 1 2]. 
+  (* Success of the tactid expected *)
+  Succeed (first [
+      solve [TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered 1 1] ; 
+      test_success
+    | test_failure]).
+
+
+  (* Failure of the tactic expected *)
+  Succeed first [
+      solve [TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered 2 1] ; 
+      test_failure 
+    | test_success].
+
+   (* Failure of the tactic expected *)
+  Succeed first [
+      solve [TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered 1 2] ; 
+      test_failure 
+    | test_success].
       
 Abort.
 
-(* second order *)
+
+(* re-ordered lemma/tactic *)
 Goal forall (cm : M)
   (H1 : In (Node {| Node_id := 1 |}) (modelElements cm))
   (H2 : In (Node {| Node_id := 2 |}) (modelElements cm)),
@@ -150,11 +260,26 @@ Goal forall (cm : M)
         TraceLink.linkPattern := lp
       |} (compute_trace T cm)
 .
+Proof.
+  idtac "Testing TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered".
+  idtac "Test case : choice between two assumptions. (2/2)".
+
 
       eexists ; eexists ; eexists. 
 
-   Succeed solve [TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered 1 1]. 
-   Fail solve [TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered 2 1]. 
+
+  (* Success of the tactid expected *)
+  Succeed (first [
+      solve [TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered 1 1] ; 
+      test_success
+    | test_failure]).
+
+
+  (* Failure of the tactic expected *)
+  Succeed first [
+      solve [TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered 2 1] ; 
+      test_failure 
+    | test_success].
       
 Abort.
 
@@ -174,14 +299,29 @@ Goal forall (cm : M)
         TraceLink.linkPattern := lp
       |} (compute_trace T cm)
 .
+Proof.
+  idtac "Testing TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered".
+  idtac "Test case : choice between two assumptions, local guard. (1/2)".
 
-      eexists ; eexists ; eexists. 
 
-   Succeed solve [TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered 2 1].       
-   Fail TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered 1 1.       
+  eexists ; eexists ; eexists. 
+
+  (* Success of the tactid expected *)
+  Succeed (first [
+      solve [TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered 2 1] ; 
+      test_success
+    | test_failure]).
+
+
+  (* Failure of the tactic expected *)
+  Succeed first [
+      TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered 1 1 ; 
+      test_failure 
+    | test_success].
+
 Abort.
 
-(* second order *)
+(* re-ordered lemma/tactic *)
 Goal forall (cm : M)
   (H1 : In (Arrow {| Arrow_id := 1 |}) (modelElements cm))
   (H2 : In (Arrow {| Arrow_id := 2 |}) (modelElements cm)),
@@ -197,11 +337,24 @@ Goal forall (cm : M)
         TraceLink.linkPattern := lp
       |} (compute_trace T cm)
 .
+Proof.
+  idtac "Testing TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered".
+  idtac "Test case : choice between two assumptions, local guard. (2/2)".
 
       eexists ; eexists ; eexists. 
 
-   Succeed solve [TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered 2 1].
-   Fail TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered 1 1.
+  (* Success of the tactid expected *)
+  Succeed (first [
+      solve [TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered 2 1] ; 
+      test_success
+    | test_failure]).
+
+
+  (* Failure of the tactic expected *)
+  Succeed first [
+       TacticsFW.in_compute_trace_inv_singleton_fw_solve_reordered 1 1 ; 
+      test_failure 
+    | test_success].
       
 Abort.
 
@@ -225,10 +378,18 @@ Goal
         TraceLink.linkPattern := lp
       |} (compute_trace T cm)
 .
+Proof.
+  idtac "Testing TacticsFW.in_compute_trace_inv_singleton_fw_solve".
+  idtac "Test case : choice between two assumptions. (1/2)".
 
       eexists ; eexists ; eexists. 
 
-      Succeed solve [TacticsFW.in_compute_trace_inv_singleton_fw_solve 1 1]. 
+  (* Success of the tactid expected *)
+  Succeed (first [
+      solve [TacticsFW.in_compute_trace_inv_singleton_fw_solve 1 1] ; 
+      test_success
+    | test_failure]).
+
 Abort.
 
 Goal 
@@ -248,10 +409,18 @@ Goal
         TraceLink.linkPattern := lp
       |} (compute_trace T cm)
 .
+Proof.
+  idtac "Testing TacticsFW.in_compute_trace_inv_singleton_fw_solve".
+  idtac "Test case : choice between two assumptions. (2/2)".
 
       eexists ; eexists ; eexists. 
 
-      Succeed solve [TacticsFW.in_compute_trace_inv_singleton_fw_solve 1 1]. 
+  (* Success of the tactid expected *)
+  Succeed (first [
+      solve [TacticsFW.in_compute_trace_inv_singleton_fw_solve 1 1] ; 
+      test_success
+    | test_failure]).
+
 Abort.
 
 (* Not tested : rules with several output patterns *)
