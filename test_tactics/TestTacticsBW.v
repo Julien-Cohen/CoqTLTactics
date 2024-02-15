c4c79eae0a9c5b6e71b60862085ecfa94b4e79d1
Julien Cohen <julien>
2024-02-15 14:06:33 +0100

    Re-modularization.

--- test_tactics/TestTacticsBW.v
+++ test_tactics/TestTacticsBW.v
@@ -1,12 +1,9 @@
 Require Import  TestUtils.
 
-
 Require TacticsBW.
 
-
 Import 
-  List Model String NotationUtils Semantics PoorTraceLink  NotationUtils Semantics.
-
+  List Model String NotationUtils Semantics PoorTraceLink  NotationUtils.
 
 Open Scope string_scope.
 
@@ -57,50 +54,6 @@ End Test1.
 
 (** Tests for tactics on links. *)
 
-Section TestOther1.
-
-(* Tactic under test : [ConcreteExpressionTools.inv_makeLink] *)
-(* Test case : *)
-
-  Import ClassMetamodel RelationalMetamodel Class2Relational.
-  Import Glue.
-  
-  Context 
-    (cm : ClassModel) 
-      (t : Attribute_t)
-      (l : list RelationalMetamodel.Link)
-      (H : ConcreteExpressions.makeLink [Attribute_K] Column_K
-             Column_reference_K
-             (fun (thisModule : Trace) (_ : nat)
-                  (m : TransformationConfiguration.SourceModel)
-                  (a : Attribute_t)
-                  (c : Column_t) =>
-                a_type <- getAttribute_typeElement a m;
-              res <-
-                ModelingSemantics.resolve thisModule "tab" Table_K
-                  (ListUtils.singleton a_type); do_glue c with res)
-             (TraceLink.drop (compute_trace Class2Relational cm)) 0 cm
-             [AttributeElement t]
-             (ColumnElement
-                {|
-                  Column_id := Attribute_id t;
-                  Column_name := Attribute_name t
-                |}) = return l).
-  
-  Goal False.
-  
-  Proof. 
-    idtac "Testing ConcreteExpressionTools.inv_makeLink.".
-    idtac "Test case : ".
-    
-    tryif 
-      ConcreteExpressionTools.inv_makeLink H
-    then test_success
-    else test_failure.
-    
-  Abort.  
-  
-End TestOther1.
 
 
 Section TestLink1.
