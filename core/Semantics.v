76d127bfc27f219cfca344190844eaae2b38cb07
Julien Cohen <Vous@exemple.com>
2024-12-17 09:33:48 +0100

    Clean.

--- core/Semantics.v
+++ core/Semantics.v
@@ -23,6 +23,8 @@ Section Semantics.
 
 Context {tc: TransformationConfiguration}.
 
+
+
 (** * Pattern matching *)
 
 (* executable *)
@@ -33,7 +35,7 @@ Definition allTuples (tr: Transformation) (sm : SourceModel) : list InputPiece :
 Definition isTuple (sm : SourceModel) ip : Prop := 
   List.incl ip sm.(modelElements).
 
-Lemma p1 : forall tr sm ip,
+Lemma prop1 : forall tr sm ip,
   List.In ip (allTuples tr sm) -> isTuple sm ip.
 Proof.
   unfold allTuples.
@@ -41,6 +43,9 @@ Proof.
 
   intros ; eapply TupleUtils.tuples_up_to_n_incl ; eassumption.
 Qed. 
+
+
+
 
 
 (* executable *)
@@ -51,7 +56,8 @@ Definition matchingRules (tr: Transformation) (sm : SourceModel) (sp: InputPiece
 Definition matchingRule (tr: Transformation) (sm : SourceModel) (sp: InputPiece) r : Prop :=
     List.In r tr.(rules) /\ UserExpressions.guard_ok r sm sp.
 
-Lemma p2 :
+(** Correct and complete *)
+Lemma prop2 :
  forall tr sm sp r,
    List.In r (matchingRules tr sm sp) <-> matchingRule tr sm sp r.
 Proof.
@@ -63,6 +69,10 @@ Proof.
   unfold evalGuard.
   tauto.
 Qed.
+
+
+
+
 
 
 (** * Building traces *)
@@ -90,7 +100,8 @@ Inductive traceElementOnPiece_rel (o: OutputPatternUnit) (sm: SourceModel) (sp: 
             linkPattern := o.(opu_link) 
           |}.
 
-Lemma p3 : 
+(** correct and complete *)
+Lemma prop3 : 
   forall o sm sp it tl,
   traceElementOnPiece_rel o sm sp it tl <-> traceElementOnPiece o sm sp it = Some tl.
 Proof.
@@ -107,6 +118,8 @@ Proof.
     exact H.
 Qed.
 
+
+
 (* executable *)
 Definition traceIterationOnPiece (r: Rule) (sm: SourceModel) (sp: InputPiece) (iter: nat) :  Trace :=
   flat_map
@@ -120,7 +133,8 @@ Inductive traceIterationOnPiece_rel (r: Rule) (sm: SourceModel) (sp: InputPiece)
         traceElementOnPiece_rel o sm sp iter tl -> 
       traceIterationOnPiece_rel r sm sp iter tl.
 
-Lemma p4 : forall r sm sp it tlk, 
+(** correct and complete *)
+Lemma prop4 : forall r sm sp it tlk, 
   traceIterationOnPiece_rel r sm sp it tlk <-> List.In tlk (traceIterationOnPiece r sm sp it).
 Proof.
   unfold traceIterationOnPiece.
@@ -128,13 +142,14 @@ Proof.
   setoid_rewrite in_flat_map.
   Search (In _ (optionToList _)).
   setoid_rewrite in_optionToList.
-  setoid_rewrite <- p3.
+  setoid_rewrite <- prop3.
   intros ; split ; intro H. 
   + inversion_clear H.
     eauto.
   + destruct H as (o & H1 & H2).
     econstructor ; eauto.
 Qed.
+
 
 
 (* executable *)
@@ -152,12 +167,13 @@ Inductive traceRuleOnPiece_rel (r: Rule) (sm: SourceModel) (sp: InputPiece) (tlk
        traceIterationOnPiece_rel r sm sp current_it tlk ->
        traceRuleOnPiece_rel r sm sp tlk.
 
-Lemma p5 : forall r sm sp tlk, 
+(* correct and complete *)
+Lemma prop5 : forall r sm sp tlk, 
   traceRuleOnPiece_rel r sm sp tlk <-> List.In tlk (traceRuleOnPiece r sm sp).
 Proof.
   unfold traceRuleOnPiece.
   setoid_rewrite in_flat_map.
-  setoid_rewrite <- p4.
+  setoid_rewrite <- prop4.
   intros ; split ; intro H.
   + inversion_clear H. 
     apply UserExpressions.p1 in H1.
@@ -174,6 +190,8 @@ Proof.
     Lia.lia.
 Qed.
 
+
+
 (* executable *)
 Definition traceTrOnPiece (tr: Transformation) (sm : SourceModel) (sp: InputPiece) : Trace :=
   flat_map 
@@ -187,13 +205,14 @@ Inductive traceTrOnPiece_rel (tr: Transformation) (sm : SourceModel) (sp: InputP
     matchingRule tr sm sp r ->
       traceTrOnPiece_rel tr sm sp tl.
 
-Lemma p6 : forall tr sm sp tlk,
+(** Correct and complete *)
+Lemma prop6 : forall tr sm sp tlk,
   traceTrOnPiece_rel tr sm sp tlk <-> List.In tlk (traceTrOnPiece tr sm sp).
 Proof.
   unfold traceTrOnPiece.
   setoid_rewrite in_flat_map.
-  setoid_rewrite <- p5.
-  setoid_rewrite p2.
+  setoid_rewrite <- prop5.
+  setoid_rewrite prop2.
   intros ; split ; intro H.
   + inversion_clear H. eauto.
   + destruct H as (r & H1 & H2).
@@ -215,12 +234,13 @@ Inductive in_trace (tr: Transformation) (sm : SourceModel) (tl:TraceLink) : Prop
       List.length sp <= tr.(arity) -> (* cohérence avec le moteur de référence *)
     in_trace tr sm tl .  
 
-Lemma p7 : forall tr sm tlk, 
+(** correct and complete *)
+Lemma prop7 : forall tr sm tlk, 
   in_trace tr sm tlk <-> List.In tlk (compute_trace tr sm).
 Proof.
   unfold compute_trace.
   setoid_rewrite in_flat_map.
-  setoid_rewrite <- p6.
+  setoid_rewrite <- prop6.
   intros ; split ; intro H.
   + inversion_clear H.
     exists sp ; split ; [ | assumption].
@@ -231,20 +251,21 @@ Proof.
     auto.
   + destruct H as (ip & H1 & H2).
     econstructor ; [ eassumption | | ].
-    - eapply p1 ; eassumption.
+    - eapply prop1 ; eassumption.
     - unfold allTuples in H1.
       Search (In _ (TupleUtils.tuples_up_to_n _ _)).
       eapply TupleUtils.tuple_length ; eassumption.
 Qed.
 
+
 Definition is_trace trans sm tra : Prop :=
   forall lk, List.In lk tra <-> in_trace trans sm lk.    
 (* On aurait pu définir ensemble en compréhension. *)
 
-Lemma p8 : forall trans sm, is_trace trans sm (compute_trace trans sm).
+Lemma prop8 : forall trans sm, is_trace trans sm (compute_trace trans sm).
 Proof.
   unfold is_trace.
-  setoid_rewrite p7.
+  setoid_rewrite prop7.
   tauto.
 Qed.
 
@@ -273,6 +294,9 @@ Qed.
 
 
 
+
+
+
 (** * Apply link part of the r.h.s of rules (uses traces) **)
 
 (* executable *)
@@ -280,7 +304,7 @@ Definition apply_link_pattern (tls:Trace) sm lk :list TargetLinkType :=
     lk.(linkPattern) (drop tls) (getIteration lk) sm (getSourcePiece lk) lk.(produced).
 
  (* predicative *)
-Inductive apply_link_pattern_rel tr sm tlk lk : Prop := 
+Inductive apply_link_pattern_rel (tr:Transformation) (sm:SourceModel) (tlk:TraceLink) (lk:TargetLinkType) : Prop := 
    | rr : 
     forall tra,
     is_trace tr sm tra ->
@@ -290,23 +314,23 @@ Inductive apply_link_pattern_rel tr sm tlk lk : Prop :=
 (* Ici on a besoin de LA trace et non pas d'UNE trace car pour deux traces différentes 
    [tlk.(linkPattern) (drop tra)] pourrait donner des résultats différents car rien ne contraint les expressions
     utilisateur.
-    Par conséquent, on ne peut pas être complètement axiomatique, on doit se reposer sur le 
-    résultat de l'application du moteur de transformation.
+    Par conséquent, on ne peut attendre une équivalence entre la sémantique axiomatique, et l'application du moteur de transformation.
   
-    Possibilité : spécifier plus précisément les propriétés d'une traces pour forcer l'unicité.
+    Possibilités :
+      1) spécifier plus précisément les propriétés d'une traces pour forcer l'unicité.
 
-    Autre possibilité : ne pas rechercher une équivalence entre la sémantique axiomatique 
+      2) ne pas rechercher une équivalence entre la sémantique axiomatique 
     et la sémantique exécutable, seulement une correction (sans complétude).
 
-    Autre possibilité : ajouter des hypothèses sur les fonctions utilisateurs permettant
+      3) ajouter des hypothèses sur les fonctions utilisateurs permettant
     de garantir l'équivalence.
 *) 
 
 
 
-(* Prove that the trace produced by the executable engine is correct (not equivalent) with respect
+(* The trace produced by the executable engine is correct (not equivalent) with respect
   to the trace defined by the relational semantics. *)
-Lemma p9 : forall tr tra sm tlk lk,
+Lemma prop9 : forall tr tra sm tlk lk,
   is_trace tr sm tra ->
   List.In lk (apply_link_pattern tra sm tlk) ->
   apply_link_pattern_rel tr sm tlk lk.
@@ -317,6 +341,7 @@ Proof.
   eassumption.
   eassumption.
 Qed.
+
 
 
 (* executable *)
@@ -331,7 +356,8 @@ Inductive applyTrLkOnModel_rel tr (sm : SourceModel) (lk: TargetLinkType) : Prop
     apply_link_pattern_rel tr sm tlk lk -> 
     applyTrLkOnModel_rel tr sm lk. 
 
-Lemma p10 : forall tr sm tra lk,
+(** Correctness (not equivalence) *)
+Lemma prop10 : forall tr sm tra lk,
    is_trace tr sm tra ->
    List.In lk (applyTrLkOnModel sm tra) ->
   applyTrLkOnModel_rel tr sm lk.
@@ -341,8 +367,11 @@ Proof.
   intros.
   destruct H0 as (k & H1 & H2).
     econstructor ; eauto.
-  eapply p9 ; eassumption. 
+  eapply prop9 ; eassumption. 
 Qed.
+
+
+
 
 
 (** * Execute **)
@@ -363,12 +392,13 @@ Definition execute (tr: Transformation) (sm : SourceModel) : TargetModel :=
 
 
 (* predicative *)
-Inductive is_produced_element tr sm : TargetElementType -> Prop :=
+Inductive is_produced_element (tr:Transformation) sm : TargetElementType -> Prop :=
     | r7 : forall a b c, 
         in_trace tr sm {| source := a; produced := b; linkPattern := c |} ->
         is_produced_element tr sm b.
 
-Lemma p11 : forall tr sm e,
+(* Correctness (not equivalence) *)
+Lemma prop11 : forall tr sm e,
   In e ( (execute tr sm).(modelElements)) ->
   is_produced_element tr sm e .
 Proof.
@@ -382,49 +412,52 @@ Proof.
   destruct k.
   simpl in H1.
   subst.
-  econstructor. apply p7. eassumption.
+  econstructor. apply prop7. eassumption.
 Qed.
 
 
+
 Inductive is_produced_link tr sm (lk:TargetLinkType): Prop :=
-    | r8 : forall tra, 
+    | r8 : forall tra,  (*pas sûr que [tra] doive être encapsulé ici au lieu d'être en paramètre de la relation. *)
         is_trace tr sm tra ->
         applyTrLkOnModel_rel tr sm lk->
         is_produced_link tr sm lk.
 
-
-Lemma p12 : forall tr sm lk,  
+(** Correctness (not equivalence) *)
+Lemma prop12 : forall tr sm lk,  
   In lk ( (execute tr sm).(modelLinks)) ->
   is_produced_link tr sm lk .
 Proof.
   unfold execute.
   simpl modelLinks.
   intros.
-  apply p10 with (tr:=tr) in H ; [ | apply p8].
+  apply prop10 with (tr:=tr) in H ; [ | apply prop8].
   econstructor.
-  eapply p8.
+  eapply prop8.
   assumption.
 Qed.
 
-(* Fixme : Distinguer 
-   1) le résultat est un résultat possible et 
-   2) le résultat est inclus dans un résultat possible. 
-    Exemple: si le moteur renvoit un modèle vide, 
-     on ne veut pas que ce soit considéré comme correct. 
+(* Attention : Distinguer 
+   1) le résultat calculé est un résultat possible.
+   2) le résultat calculé est inclus dans un résultat possible. 
+        (Exemple: si le moteur renvoie un modèle vide, on ne veut pas que ce soit considéré comme correct.) 
+   3) le résultat calculé contient tous les résultats possibles.
+        (Exemple: on voudrait que le moteur renvoie les liens calculés pour une trace, pas pour toutes les traces possibles.) 
+
+  Question : un moteur peut-il utiliser plusieurs traces différentes (parallélisation) ?
+    
 *)
 
-Definition is_result tr sm tm: Prop :=
+Definition is_result (tr:Transformation) (sm:SourceModel) (tm:TargetModel): Prop :=
   (forall e, (List.In e tm.(modelElements) <-> is_produced_element tr sm e)) /\ 
   (forall lk, (List.In lk tm.(modelLinks) <-> is_produced_link tr sm lk)).
-(* On aurait pu définir un ensemble en compréhension. *)
 
-(*Lemma p13 : forall tr sm,  is_result tr sm (execute tr sm).
+
+(** Correctness *)
+Lemma prop13 : forall tr sm,  is_result tr sm (execute tr sm).
 Proof.
-  unfold is_result.
-  intros tr sm.
-  split.
-  +
-Qed.*)
+Abort.
+
 
 End Semantics.
 
