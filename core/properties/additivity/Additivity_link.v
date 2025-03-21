4165e8265f94f59960c9aec40fcd4b7a9b675109
Zheng Cheng <cz0590@gmail.com>
2025-03-21 11:21:43 +0800

    adapt Additivity (link)

--- core/properties/additivity/Additivity_link.v
+++ core/properties/additivity/Additivity_link.v
@@ -11,11 +11,17 @@ Require Import Lia.
 Require Import FunctionalExtensionality.
 Require Import AxiomaticSemantics.
 
+
+Require Import core.properties.additivity.Moore2Mealy_Additivity_link_witness.
+Require Import core.properties.additivity.sampleMoore_additivity_link.
+
+
 (*************************************************************)
 (** * Additivity in Rule context (Link)                      *)
 (** * Using operational semantics                            *)
 (*************************************************************)
 
+Require Import Coq.Logic.Classical_Pred_Type.
 
 
 Definition Transformation_incl_rules {tc: TransformationConfiguration} (t1 t2: Transformation) : Prop :=
@@ -27,109 +33,53 @@ Definition Rule_Additivity_Link {tc: TransformationConfiguration} :=
       (Transformation_incl_rules t1 t2 -> 
           incl (execute t1 sm).(modelLinks) (execute t2 sm).(modelLinks)). 
 
-Theorem forall_Additivity_Rules_Link {tc: TransformationConfiguration} : Rule_Additivity_Link.
+Lemma Moore2Mealy_non_additivity_link_contrapos:
+  exists sm : SourceModel,
+  ~
+  (Transformation_incl_rules Moore2Mealy_t1
+    Moore2Mealy_t2 ->
+  incl (modelLinks (execute Moore2Mealy_t1 sm))
+    (modelLinks (execute Moore2Mealy_t2 sm))).
+Proof.
+  exists Moore_m.
+  unfold not.
+  intro.
+  assert (Transformation_incl_rules Moore2Mealy_t1 Moore2Mealy_t2).
+  {
+    unfold Moore2Mealy_t1. unfold Moore2Mealy_t2.
+    unfold Transformation_incl_rules.
+    simpl.
+    crush.
+  }
+  specialize (H H0).
+  clear H0.
+  unfold not.
+  unfold incl in H.
+  simpl in H.
+  apply ex_not_not_all in H.
+  assumption.
+  compute.
+  eexists.
+  intro.
+  destruct H0.
+  ++ left.
+     reflexivity.
+  ++ crush.
+  ++ assumption.
+Qed.
+
+Lemma not_additivity_link : ~ (Rule_Additivity_Link).
 Proof.
   unfold Rule_Additivity_Link.
-  simpl.
-  unfold incl.
-  unfold compute_trace, produced_elements, applyTrLkOnModel .
-  unfold traceTrOnPiece.
-  intros ? ? ? ? ?.
-  intro.
-  apply in_flat_map in H0.
-  destruct H0.
-  destruct H0.
-  apply in_flat_map.
-  exists x.
-  split.
-  + apply in_flat_map.
-    apply in_flat_map in H0. destruct H0. exists x0.
-    destruct H0.
-    unfold allTuples in *.
-    split.
-    ++ unfold Transformation_incl_rules in H. destruct H. rewrite <- H. assumption.
-    ++ apply in_flat_map in H2. destruct H2. destruct H2.
-    apply in_flat_map.
-    exists x1. split.
-    +++ unfold matchingRules in *.
-        apply filter_In in H2.
-        apply filter_In.
-        unfold Transformation_incl_rules in H. destruct H.
-        destruct H2.
-        specialize (H4 x1 H2).
-        split; assumption.
-    +++ assumption.
-  +
-Abort.
-
-
-(*
-  repeat rewrite in_flat_map.
-  intro H0.
-  apply in_flat_map in H0. destruct H0 as (r1, (H0, H1)). 
-  rewrite map_flat_map in H1.
-  apply in_flat_map in H1. destruct H1 as (r2, (H1,H2)).
-  apply filter_In in H1. destruct H1.
-  destruct H as (H4, H5).
-  apply in_flat_map. exists r1.
-  split.
-  * unfold allTuples.
-    rewrite <- H4.
-    assumption.
-  * rewrite map_flat_map.
-    apply in_flat_map.
-    specialize (H5 r2 H1).
-    exists r2.
-    split.
-    + apply filter_In.
-      split; assumption.
-    + assumption.
-Qed.
-*)
-
-
-
-
-(**
-
-Definition Transformation_incl_rules'' {tc: TransformationConfiguration} (t1 t2: Transformation) : Prop :=
-  (t1.(arity) = t2.(arity)) /\ 
-  subseq t1.(rules) t2.(rules).
-
-Lemma tr_incl_equiv:
-  forall (tc: TransformationConfiguration) t1 t2,
-    Transformation_incl_rules'' t1 t2 -> Transformation_incl_rules''' t1 t2.
-Proof.
-intros.
-destruct  H.
-unfold Transformation_incl_rules'''.
-split. 
-* assumption.
-* intro.
-  induction H0.
-  + intros.
-    contradiction.
-  + intros.
-    simpl in H1.
-    simpl.
-    destruct H1.
-    - left. assumption.
-    - right. auto 2.
-  + intros.
-    simpl.
-    right.
-    auto 2.
+  apply ex_not_not_all.
+  exists Moore2Mealy_t1.
+  apply ex_not_not_all.
+  exists Moore2Mealy_t2.
+  apply ex_not_not_all.
+  apply Moore2Mealy_non_additivity_link_contrapos.
 Qed.
 
-Theorem additivity_rules :
-forall (tc: TransformationConfiguration) (t1 t2: Transformation) (sm: SourceModel),
-  (Transformation_incl_rules'' t1 t2 -> 
-    incl (execute t1 sm).(modelElements)  (execute t2 sm).(modelElements)).
-Proof.
- intros.
- specialize (tr_incl_equiv tc t1 t2 H).
- specialize (additivity_rules_general_operational tc t1 t2).
- auto.
-Qed.
 
-*)
+
+
+
---
+++ core/properties/additivity/sampleMoore_additivity_link.v
@@ -0,0 +1,29 @@
+
+		 
+ Require Import List.
+ Require Import core.Model.
+ Require Import String.
+ Require Import transformations.Moore2Mealy.Moore.
+ Open Scope string_scope.
+ 
+Import Glue.
+
+(* Moore models as counterexample to disprove injectivity of CoqTL *)
+
+
+ Definition Moore_m : Model Moore.MM :=
+    (Build_Model Moore.MM
+        (
+            (Transition (Build_Transition_t 0 "0000")) :: 
+            (State (Build_State_t  (Id.Id "S0000") "1111")) :: 
+            (State (Build_State_t  (Id.Id "S1111") "0000")) ::  
+            nil
+        )
+        (
+            (TransitionSource (Build_Glue _ _ (Build_Transition_t 0 "0000") (Build_State_t  (Id.Id "S1111") "0000"))) ::
+            (TransitionTarget (Build_Glue _ _ (Build_Transition_t 0 "0000") (Build_State_t  (Id.Id "S0000") "1111"))) ::
+            nil
+        )
+).
+ 
+
