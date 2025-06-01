98b8c54bb9de7fd66de0339c143365bca1a2193f
Massimo Tisi <massimo.tisi@imt-atlantique.fr>
2025-06-01 10:03:07 +0200

    Invertibility

---
+++ core/properties/invertibility/invertibility.v
@@ -0,0 +1,37 @@
+
+Require Import String.
+Require Import EqNat.
+Require Import List.
+Require Import PeanoNat.
+Require Import Lia.
+Require Import FunctionalExtensionality.
+
+Require Import core.Semantics.
+Require Import core.Syntax.
+Require Import core.Model.
+Require Import core.TransformationConfiguration.
+Require        core.Certification.
+Require Import core.utils.Utils.
+
+Require Import core.modeling.ConcreteSyntax.
+Require Import core.modeling.ModelingSemantics.
+Require Import core.modeling.ModelingMetamodel.
+Require Import core.modeling.ConcreteExpressions.
+Require Import core.modeling.Parser.
+
+Require Import core.properties.surjectivity.Moore2Mealy_surjectivity_model_witness.
+Require Import core.properties.surjectivity.sampleMealy_surjectivity_model.
+
+(*************************************************************)
+(** * Surjectivity of CoqTL (Model)                          *)
+(** * Using operational semantics                            *)
+(*************************************************************)
+
+Definition Right_Invertibility {tc:TransformationConfiguration} (tr:Transformation) :=
+    exists (tr_inv: Transformation), forall (sm:SourceModel) (tm:TargetModel),
+      (execute tr sm) = tm -> (execute tr_inv tm) = sm.
+
+Definition Left_Invertibility {tc:TransformationConfiguration} (tr:Transformation) :=
+    exists (tr_inv: Transformation), forall (sm:SourceModel) (tm:TargetModel),
+      (execute tr_inv tm) = sm -> (execute tr sm) = tm.
+
