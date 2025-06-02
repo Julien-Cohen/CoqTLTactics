5348060c0be57439aecc5dc28b20381fedfc8de7
Julien Cohen <Vous@exemple.com>
2025-06-02 16:36:26 +0200

    Proof that forward and backward traceability hold (from results on elements and links).

--- core/properties/backward_traceability/backward_traceability.v
+++ core/properties/backward_traceability/backward_traceability.v
@@ -1,2 +1,10 @@
+From core.properties.backward_traceability 
+  Require Import backward_traceability_elem backward_traceability_link.
+
+Import TransformationConfiguration Syntax.
+
 Theorem Backward_Traceability {tc:TransformationConfiguration} :
-    forall (tr: Transformation), forall_Backward_Traceability_elem tr /\ forall_Backward_Traceability_links.
+    forall (tr: Transformation), Backward_Traceability_elem tr /\ Backward_Traceability_link tr.
+Proof.
+  intro tr ; split ; [ apply forall_Backward_Traceability_elem | apply forall_Backward_Traceability_links ].
+Qed.
--- core/properties/forward_traceability/Forward_Traceability.v
+++ core/properties/forward_traceability/Forward_Traceability.v
@@ -1,2 +1,10 @@
+From core.properties.forward_traceability 
+  Require Import Forward_Traceability_elem Forward_Traceability_link.
+
+Import TransformationConfiguration Syntax.
+
 Theorem Forward_Traceability {tc:TransformationConfiguration} :
-    forall_Forward_Traceability_elem /\ forall_Forward_Traceability_links.
+    forall (tr: Transformation), Forward_Traceability_elem tr /\ Forward_Traceability_links tr.
+Proof.
+  intro tr ; split ; [ apply forall_Forward_Traceability_elem | apply forall_Forward_Traceability_links ].
+Qed.
